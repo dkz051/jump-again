62a3945ef5cbc07aac086a74575345131b3b56ef
刘润达 <liurd18@mails.tsinghua.edu.cn>
2020-05-18 10:38:53 +0800

    modify

--- renderer.vhd
+++ renderer.vhd
@@ -68,11 +68,9 @@ begin
 							cnt3 <= cnt3_x0;
 						else
 							if cnt3 = 2 then 
+								readFrom <= readFrom +  1;
 								cnt3 <= 0;
 							else	
-								if cnt3 = 1 then	
-									readFrom <= readFrom + 1;
-								end if;
 								cnt3 <= cnt3 + 1;
 							end if;
 						end if;
